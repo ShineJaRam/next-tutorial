6d8bc955fb3a89d6b5ed7f3b2b25782365012bba
임수현 <suhuyn19@naver.com>
2021-09-15 16:36:35 +0900

    fix: classname 라이브러리 삭제

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,18 +1,8 @@
 import styles from './alert.module.css';
-import cn from 'classnames';
 import { ReactNode } from 'react';
 
 const Alert = ({ children, type }: { children: ReactNode; type: string }) => {
-  return (
-    <div
-      className={cn({
-        [styles.success]: type === 'success',
-        [styles.error]: type === 'error',
-      })}
-    >
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default Alert;
