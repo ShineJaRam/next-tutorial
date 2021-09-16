31761c1583a64bbf5ac52be3d38f7013743e3002
임수현 <suhuyn19@naver.com>
2021-09-16 09:18:40 +0900

    fix: sitemap 빌드 오류 수정

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,7 +1,9 @@
 //pages/sitemap.xml.js
+import { GetServerSideProps, NextApiRequest } from 'next';
+
 const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
 
-function generateSiteMap(posts) {
+function generateSiteMap(posts: { index: number; id: string | string[] }[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
@@ -12,7 +14,7 @@ function generateSiteMap(posts) {
        <loc>https://jsonplaceholder.typicode.com/guide</loc>
      </url>
      ${posts
-       .map(({ id }) => {
+       .map(({ id }: { id: string | string[] }) => {
          return `
        <url>
            <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
@@ -28,7 +30,7 @@ function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
 
-export async function getServerSideProps({ res }) {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   // We make an API call to gather the URLs for our site
   const request = await fetch(EXTERNAL_DATA_URL);
   const posts = await request.json();
@@ -44,6 +46,6 @@ export async function getServerSideProps({ res }) {
   return {
     props: {},
   };
-}
+};
 
 export default SiteMap;
